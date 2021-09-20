fed0b846ed6dd016b7021581c69b9d8a470a513b
Don Carlos Zendejas <czende@hotmail.com>
2021-09-20 16:39:41 -0500

    Update main.go

--- main.go
+++ main.go
@@ -24,12 +24,12 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
+	router.GET("/inicio", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
 	
-//	router.GET("/v1/multiplica", netHandle(handleDBGettokenizedcards, nil))
+	router.GET("/v1/albums", getAlbums)
 	router.Run(":" + port)
 /*
 	var r = net.GetRouter()
@@ -42,8 +42,25 @@ func main() {
 }
 //termin ortiginall
 
+ 
 
-func handleDBGettokenizedcards(w http.ResponseWriter, r *http.Request) {
+// album represents data about a record album.
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
 
-	fmt.Print("uno");					
+// albums slice to seed record album data.
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, albums)
 }
