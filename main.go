3fd95ea00899a7d80cecb1b07c3ecbeb26c07679
Don Carlos Zendejas <czende@hotmail.com>
2021-09-21 10:34:15 -0500

    Update main.go
    remove refrences to the original solution

--- main.go
+++ main.go
@@ -31,7 +31,6 @@ func main() {
 	})
 
 	
-	router.GET("/v1/albums", getAlbums)
 	
 	router.GET("v1/multiplica/:numero1/:numero2", getMultiplicaByID)
 	
@@ -43,25 +42,12 @@ func main() {
 
  
 
-// album represents data about a record album.
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
-}
 
-// albums slice to seed record album data.
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
 
-// album represents data about a record album.
+// multiplica represents data about multiplicacion.
 type multiplica struct {
     Status     string  `json:"status"`
-    Resultado  string  `jsonresultado"`
+    Resultado  string  `json:"resultado"`
 
 }
 
@@ -70,13 +56,8 @@ var resultados = []multiplica{
 
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
-}
 
-
-// getAlbums responds with the list of all albums as JSON.
+// getMultiplicaByID responds with the stauts and the result as JSON.
 func getMultiplicaByID(c *gin.Context) {
 	 elemento1 := c.Param("numero1")
 	 elemento2 := c.Param("numero2")
