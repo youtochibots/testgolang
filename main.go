44d1bf3f11900977913b1bee019b259dec5f474b
Don Carlos Zendejas <czende@hotmail.com>
2021-09-28 11:28:22 -0500

    Update main.go
    add back the logic to add a fiel in a github repository

--- main.go
+++ main.go
@@ -12,7 +12,20 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 	"strconv"
 	"fmt"
+	
+// modules for the github repository functionality and the in memory repository 
+        billy "github.com/go-git/go-billy/v5"
+        memfs "github.com/go-git/go-billy/v5/memfs"
+        git "github.com/go-git/go-git/v5"
+        httpgit "github.com/go-git/go-git/v5/plumbing/transport/http"
+        memory "github.com/go-git/go-git/v5/storage/memory"	
 )
+
+
+// variables for the the in memory repository  and the filesystem to handle internally the local git repo
+var storer *memory.Storage
+var fs billy.Filesystem
+
 
 func main() {
 	port := os.Getenv("PORT")
@@ -35,6 +48,8 @@ func main() {
 	
 	
 	router.GET("v1/multiplica/:numero1/:numero2", getMultiplicaByID)
+	
+	router.GET("v2/addFileGit/:nombrearchivo/:numero2", getAddFileGit)
 	
 	router.Run(":" + port)
 
@@ -98,4 +113,79 @@ func getMultiplicaByID(c *gin.Context) {
 //	response:= json.NewEncoder(w).Encode(map[string]string{"status": "OK"})	
 	
         c.IndentedJSON(http.StatusOK, resultados)
+}
+
+
+//func addInGit(filenombre string )  bolean{
+func addInGit() {
+	
+	fmt.Println("addInGit   delcare in memory")
+	
+        storer = memory.NewStorage()
+        fs = memfs.New()
+        fmt.Println("addInGit   set auth")
+        // Authentication
+        auth := &httpgit.BasicAuth{
+                Username: "youtochibots",
+                Password: "Imposibl",
+        }
+
+	fmt.Println("addInGit   define github repository and login ")
+        repository := "https://github.com/youtochibots/bot.git"
+        r, err := git.Clone(storer, fs, &git.CloneOptions{
+                URL:  repository,
+//                Auth: auth,
+        })
+	
+	fmt.Println("addInGit   login in ok")
+	
+        if err != nil {
+                fmt.Printf("%v", err)
+                return 
+        }
+        fmt.Println("Repository cloned")
+
+        w, err := r.Worktree()
+        if err != nil {
+                fmt.Printf("%v", err)
+                return 
+        }
+
+	fmt.Println("addInGit   create new file")
+        // Create new file
+        filePath := "my-new-ififif.txt"
+        newFile, err := fs.Create(filePath)
+        if err != nil {
+                return 
+        }
+        newFile.Write([]byte("My new file carlos"))
+        newFile.Close()
+
+        // Run git status before adding the file to the worktree
+        fmt.Println(w.Status())
+
+	fmt.Println("addInGit   git add the file")
+        // git add $filePath
+        w.Add(filePath)
+
+        // Run git status after the file has been added adding to the worktree
+        fmt.Println(w.Status())
+
+	fmt.Println("addInGit   git commit")
+        // git commit -m $message
+        w.Commit("Added my new file", &git.CommitOptions{})
+
+	
+	fmt.Println("addInGit   git push")
+        //Push the code to the remote
+        err = r.Push(&git.PushOptions{
+                RemoteName: "origin",
+                Auth:       auth,
+        })
+        if err != nil {
+                fmt.Printf("%v", err)		
+                return 
+        }
+        fmt.Println("Remote updated.", filePath)
+        return
 }
